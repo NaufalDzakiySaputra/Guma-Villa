16983b70d68766c5bdd2cc74d71f25c3d346ba9f
naufaldzakiy <dzakiynaufal206@gmail.com>
2025-12-19 02:30:05 +0700

    update lagi

--- app/Models/Payments.php
+++ app/Models/Payments.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Relations\PaymentsRelation;
-class Payment extends Model
+class Payments extends Model
 {
     use HasFactory;
     use PaymentsRelation;
