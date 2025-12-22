2e0c3b51b02e41019b9f17666a263cb4f6505e97
Joanna Mareta <joannamareta07@gmail.com>
2025-12-23 04:46:06 +0700

    commit revisi

--- resources/views/admin/packages/index.blade.php
+++ resources/views/admin/packages/index.blade.php
@@ -1,57 +1,44 @@
 <!DOCTYPE html>
-<html lang="id">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Daftar Menu</title>
-    <link rel="stylesheet" href="{{ asset('css/menus.css') }}">
+    <title>Daftar Packages</title>
+    <link rel="stylesheet" href="{{ asset('css/packages.css') }}">
 </head>
 <body>
+
 <div class="container">
-    @if(session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
-    @endif
+    <h1>Daftar Packages</h1>
 
-    <div class="header-wrapper">
-        <h4>Daftar Menu</h4>
-        <a href="{{ route('menus.create') }}" class="btn btn-primary">+ Tambah Menu</a>
-    </div>
+    <a href="{{ route('packages.create') }}" class="btn">➕ Tambah Package</a>
 
-    <div class="menu-grid">
-        @forelse ($menus as $menu)
-            <div class="card">
-                @if($menu->image_path)
-                    <img src="{{ asset($menu->image_path) }}" class="card-img">
-                @else
-                    <div class="no-image">Tidak ada gambar</div>
-                @endif
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Harga</th>
+            <th>Service Type</th>
+            <th>Aksi</th>
+        </tr>
 
-                <div class="card-body">
-                    <h5>{{ $menu->name }}</h5>
-                    <p>{{ $menu->description ?? 'Tidak ada deskripsi' }}</p>
+        @foreach ($packages as $package)
+        <tr>
+            <td>{{ $package->nama }}</td>
+            <td>Rp {{ number_format($package->price) }}</td>
+            <td>{{ ucfirst($package->service_type) }}</td>
+            <td>
+                <a href="{{ route('packages.edit', $package->id) }}" class="btn">Edit</a>
 
-                    <div class="price-container">
-                        @if($menu->discount)
-                            @php $final = $menu->price - ($menu->price * ($menu->discount / 100)); @endphp
-                            <div class="price-old">Rp {{ number_format($menu->price,2,',','.') }}</div>
-                            <div class="price-new">Rp {{ number_format($final,2,',','.') }}</div>
-                        @else
-                            <div class="price-regular">Rp {{ number_format($menu->price,2,',','.') }}</div>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="card-footer">
-                    <a href="{{ route('menus.edit', $menu->id) }}" class="btn btn-warning">Edit</a>
-                    <form action="{{ route('menus.destroy', $menu->id) }}" method="POST" onsubmit="return confirm('Yakin hapus?')">
-                        @csrf @method('DELETE')
-                        <button class="btn btn-danger">Hapus</button>
-                    </form>
-                </div>
-            </div>
-        @empty
-            <p>Menu belum tersedia</p>
-        @endforelse
-    </div>
+                <form action="{{ route('packages.destroy', $package->id) }}"
+                      method="POST"
+                      style="display:inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="delete">Hapus</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
 </div>
+
 </body>
 </html>
