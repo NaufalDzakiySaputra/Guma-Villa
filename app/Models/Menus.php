2018cf78d56181e56d40c4a7072f778af2597e38
helmiarifzulfikar1424 <elrifkar07@gmail.com>
2025-12-23 11:26:43 +0700

    elllllll

--- app/Models/Menus.php
+++ app/Models/Menus.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Relations\MenusRelation; // Import the MenusRelation trait
+use App\Relations\MenusRelation;
 
 class Menus extends Model
 {
-    use HasFactory, MenusRelation; // 
+    use HasFactory, MenusRelation;
 
     protected $table = 'menus';
 
@@ -18,10 +18,30 @@ class Menus extends Model
         'price',
         'discount',
         'image_path',
+        'user_id', // Pastikan user_id juga bisa diisi
     ];
 
     protected $casts = [
         'price' => 'decimal:2',
         'discount' => 'decimal:2',
     ];
+
+    //Rumus Hitung Harga Setelah Diskon
+    public function getHargaDiskonAttribute()
+    {
+        // Harga dikurangi (Harga x Diskon / 100)
+        return $this->price - ($this->price * ($this->discount / 100));
+    }
+
+    // Rumus Format Rupiah untuk Harga Asli
+    public function getFormatHargaAttribute()
+    {
+        return 'Rp ' . number_format($this->price, 0, ',', '.');
+    }
+
+    // Rumus Format Rupiah untuk Harga Setelah Diskon
+    public function getFormatDiskonAttribute()
+    {
+        return 'Rp ' . number_format($this->harga_diskon, 0, ',', '.');
+    }
 }
--- resources/views/admin/menus/edit.blade.php
+++ resources/views/admin/menus/edit.blade.php
@@ -1,53 +1,48 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <title>Edit Menu</title>
-    <link rel="stylesheet" href="{{ asset('css/menus.css') }}">
-</head>
-<body>
-<div class="container">
+<link rel="stylesheet" href="{{ asset('css/menus.css') }}">
+
+<div class="container form-container">
     <h4>Edit Menu</h4>
+    
     <form action="{{ route('menus.update', $menu->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf @method('PUT')
+        @csrf 
+        @method('PUT')
         
         <div class="form-group">
             <label>Nama Menu</label>
-            <input type="text" name="name" class="input-field" value="{{ old('name', $menu->name) }}" required>
+            <input type="text" name="name" class="input-field" value="{{ $menu->name }}">
         </div>
 
         <div class="form-group">
             <label>Deskripsi</label>
-            <textarea name="description" class="input-field" rows="4">{{ old('description', $menu->description) }}</textarea>
+            <textarea name="description" class="input-field" rows="4">{{ $menu->description }}</textarea>
         </div>
 
         <div class="form-group">
             <label>Harga</label>
-            <input type="number" name="price" class="input-field" value="{{ old('price', $menu->price) }}" required>
+            <input type="number" name="price" class="input-field" value="{{ $menu->price }}">
         </div>
 
         <div class="form-group">
             <label>Diskon (%)</label>
-            <input type="number" name="discount" class="input-field" value="{{ old('discount', $menu->discount) }}">
+            <input type="number" name="discount" class="input-field" value="{{ $menu->discount }}">
         </div>
 
         <div class="form-group">
-            <label>Gambar Saat Ini</label><br>
+            <label>Gambar Saat Ini</label>
             @if($menu->image_path)
-                <img src="{{ asset($menu->image_path) }}" class="img-preview">
-            @else
-                <p>Belum ada gambar</p>
+                <img src="{{ asset($menu->image_path) }}" class="img-edit-preview">
             @endif
-        </div>
-
-        <div class="form-group">
-            <label>Ganti Gambar</label>
             <input type="file" name="image" class="input-field">
         </div>
 
-        <button type="submit" class="btn btn-primary">Update</button>
-        <a href="{{ route('menus.index') }}" class="btn btn-secondary">Kembali</a>
+        <div class="form-actions">
+            <button type="submit" class="btn btn-primary btn-fixed">
+                Update
+            </button>
+            
+            <a href="{{ route('menus.index') }}" class="btn btn-secondary btn-fixed">
+                Batal
+            </a>
+        </div>
     </form>
 </div>
-</body>
-</html>
